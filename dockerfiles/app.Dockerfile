53f390838e3ae22882971c3ea1d84afc376b9e59
Ethan Knox <ethan@knox.dev>
2025-05-16 14:01:29 -0400

    psycopg2-binary won't work across archs. need to build psycopg2 from source

--- dockerfiles/app.Dockerfile
+++ dockerfiles/app.Dockerfile
@@ -4,7 +4,12 @@ ENV PYTHONPATH=/src
 ARG ENVIRONMENT=development
 ENV ENVIRONMENT=${ENVIRONMENT}
 COPY . /src
-RUN apt-get update && apt-get install -y libmagic1
+# this is for image and file type detection
+RUN apt update && apt-get install -y libmagic1 \
+# for psycopg2 build - because we are using different architectures, this must be built from source
+# instead of psycopg2-binary
+libpq-dev \
+build-essential
 WORKDIR /src
 RUN uv sync
 CMD /bin/bash -c "./app_startup/${ENVIRONMENT}.sh"
--- opentofu/rds.tf
+++ opentofu/rds.tf
@@ -4,9 +4,9 @@ resource "aws_db_instance" "main" {
   engine_version          = "15"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
-  username                = var.db_username
+  username                = "postgres"
   password                = random_password.db_password.result
-  db_name                 = "appdb"
+  db_name                 = "postgres"
   vpc_security_group_ids  = [aws_security_group.rds.id]
   db_subnet_group_name    = aws_db_subnet_group.pg.name
   skip_final_snapshot     = true
