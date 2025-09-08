edfb5e09009ed50cd8c697caae8fa04912734c84
Ethan Knox <ethan@knox.dev>
2025-09-08 13:52:42 -0400

    use full path

--- app/webapp/templatetags/vite_assets.py
+++ app/webapp/templatetags/vite_assets.py
@@ -4,7 +4,10 @@ from django import template
 from django.templatetags.static import static
 from django.core.cache import cache
 from webapp.settings import IS_PRODUCTION
+from webapp.logging import get_logger
 
+
+logger = get_logger(__name__)
 register = template.Library()
 
 
@@ -16,14 +19,20 @@ def vite_asset(asset_name):
     # Check cache first
     cache_key = f"vite_manifest_{asset_name}"
     if cached_result := cache.get(cache_key):
+        logger.info(f"Cache hit for {cache_key}")
         return cached_result
     if IS_PRODUCTION:
         try:
-            manifest = requests.get("/static/libraries/.vite/manifest.json", timeout=2).json()
+            logger.info("Fetching Vite manifest for production asset")
+            fullpath = "https://static.opensailor.org/static/libraries/.vite/manifest.json"
+            manifest = requests.get(fullpath, timeout=2).json()
             entry = manifest.get("src/main.js", {})
             if "file" in entry:
+                logger.info(f"Caching asset {entry['file']} for {cache_key}")
                 result = static(f'libraries/{entry["file"]}')
                 cache.set(cache_key, result, 3600 * 24 * 30)  # Cache for 30 days
+                logger.info(f"Cached asset {entry['file']} for {cache_key}")
+                logger.info(f"Returning production asset {result}")
                 return result
         except (
             requests.RequestException,
@@ -31,5 +40,7 @@ def vite_asset(asset_name):
             KeyError,
             FileNotFoundError,
         ):
+            logger.error("Error fetching or parsing Vite manifest, falling back to unversioned asset", exc_info=True)
             pass
+    logger.info("Returning unversioned asset for development or fallback")
     return static(f"libraries/{asset_name}")
